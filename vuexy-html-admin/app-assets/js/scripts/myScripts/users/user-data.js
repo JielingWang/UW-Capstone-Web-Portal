db91abcef79e3e8380bce9f45c0395caa9993c49
jielingw <jielingw@uw.edu>
2020-04-03 17:16:20 -0700

    By Apr 3

--- vuexy-html-admin/app-assets/js/scripts/myScripts/users/user-data.js
+++ vuexy-html-admin/app-assets/js/scripts/myScripts/users/user-data.js
@@ -1,5 +1,5 @@
 const baseURL = "http://localhost:3000/api/";
-var uer_id = "5e63127145f8e019d1f26ddc";
+var user_id = "5e63127145f8e019d1f26ddc";
 
 
 // Template POst request Ajax call
@@ -56,7 +56,7 @@ var makeGetRequest = function(url, onSuccess, onFailure) {
 };
 
 
-$(document).on('click', '#add_item_button', function() {
+// $(document).on('click', '#add_item_button', function() {
     // var JSON_data = {
     //     "Name": null,
     //     "email": null,
@@ -74,49 +74,60 @@ $(document).on('click', '#add_item_button', function() {
     // var category = $('input[name=category]').val().replace(/(^\s*)|(\s*$)/g, "");
     // JSON_data += '"category":' + '"' + category + '",';
 
-})
+// })
 
-function uploadFiles_without_HTML_FORMS()
-  {
+$(document).on('click', '#confirm_item', function uploadFiles_without_HTML_FORMS() {
+    alert('send data to database');
     var formData = new FormData();
 
     //this is the JSON Object we are sending to the server
     var JSON_toServer = {
         "userID_ref": null, 
         "OrderType": null, 
-        "OrderInfo":null,  //this is where we are going to put JSON_OrderInfo_inForm JSON object, but we will convert JSON_OrderInfo_inForm JSON object to string to send to server
-        "OrderStatus":null, 
-        "ChatInfo":null,
-        "assignedTo":null
+        "OrderInfo": null,  //this is where we are going to put JSON_OrderInfo_inForm JSON object, but we will convert JSON_OrderInfo_inForm JSON object to string to send to server
+        "OrderStatus": null, 
+        "ChatInfo": null,
+        "assignedTo": null
     }
 
     //this is the information, you need to collect from the frontend. Field in the JSON object will be different according to the type of Form user is submitting
     var JSON_OrderInfo_inForm = {
-        "Name": null,
-        "age": null,
-        "country":null
+        "ReimburseFor": null,
+        "Individual": null,
+        "PaymentMethod": null,
+        "ExpenseDescription": null,
+        "BusinessPurpose": null,
+        "Category": null,
+        "Amount": null,
+        "TaxPaid": null,
+        "BudgetNum": null
     }
 
     //lets setup JSON_OrderInfo_inForm JSON Object according to the information from the frontend form --> actual field names varies according to your frontend form
-    JSON_OrderInfo_inForm.Name = "Kalana Sahabandu";
-    JSON_OrderInfo_inForm.age = "25";
-    JSON_OrderInfo_inForm.country = "Sri Lanka";
+    JSON_OrderInfo_inForm.ReimburseFor = "test";
+    JSON_OrderInfo_inForm.Individual = "";
+    JSON_OrderInfo_inForm.PaymentMethod = "";
+    JSON_OrderInfo_inForm.ExpenseDescription = "";
+    JSON_OrderInfo_inForm.BusinessPurpose = "";
+    JSON_OrderInfo_inForm.Category = "test";
+    JSON_OrderInfo_inForm.Amount = "12345";
+    JSON_OrderInfo_inForm.TaxPaid = "";
+    JSON_OrderInfo_inForm.BudgetNum = "888";
 
     //now lets set up the JSON_toServer JSON Object
-    JSON_toServer.userID_ref = user_id;
+    JSON_toServer.userID_ref = user_id;  // 5e63127145f8e019d1f26ddc
     JSON_toServer.OrderType = "Test Orderzz";
     JSON_toServer.OrderInfo = JSON.stringify(JSON_OrderInfo_inForm);
     JSON_toServer.OrderStatus = "Submitted"; //leave this as Submitted, this represent current status of the Order. Example Order Status: Submitted, approved, etc:
     JSON_toServer.ChatInfo = "TEST CHAT INFO"; //leaving this empty since there's no chat when user upload a order first
-    JSON_toServer.assignedTo = null; //leaving this as null since there's no one assigned when a user upload/submit a order.  
+    JSON_toServer.assignedTo = null; //leaving this as null since there's no one assigned when a user upload/submit a order.
 
 
-    var fileSelect = document.getElementById("fileField1");
-    console.log("HErHEWr");
-    for(var x=0;x<fileSelect.files.length;x++)
-    {
-        formData.append(fileSelect.files[x].name,fileSelect.files[x]);
-    }
+    // var fileSelect = document.getElementById("fileField1");
+    // console.log("HErHEWr");
+    // for(var x = 0; x < fileSelect.files.length; x++) {
+    //     formData.append(fileSelect.files[x].name, fileSelect.files[x]);
+    // }
         //formData.append("files", fileSelect.files[x]); //"files" should stay as it is, becuase this is how server can identify files from the JSON information, when it get this HTTP request"
 
 
@@ -133,4 +144,5 @@ function uploadFiles_without_HTML_FORMS()
     request.open('POST', baseURL + "uploadOrder");
     console.log(formData);
     request.send(formData);
-  }
+});
+
